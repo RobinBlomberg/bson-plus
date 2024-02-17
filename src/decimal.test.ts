c6688985db8a7c79155456a491b5990ea7fbe2fd
Robin Blomberg <hello@robinblomberg.se>
2024-02-17 15:13:02 +0100

    feat(decimal): support negative decimals

--- src/decimal.test.ts
+++ src/decimal.test.ts
@@ -5,12 +5,16 @@ import { readDecimal, writeDecimal } from './decimal.js';
 const dataView = new DataView(new ArrayBuffer(32));
 
 const decimal = (input: number) => {
-  writeDecimal([dataView, 0], input);
-  const output = readDecimal([dataView, 0]);
-  strictEqual(output, input);
+  for (const value of [input, -input]) {
+    writeDecimal([dataView, 0], value);
+    const output = readDecimal([dataView, 0]);
+    strictEqual(output, Object.is(value, -0) ? 0 : value);
+  }
 };
 
 test('decimal', () => {
+  decimal(0);
+  decimal(1);
   decimal(123.456);
   decimal(3.141_592_653_589_793);
   decimal(9_007_199_254_740_991);
--- src/decimal.ts
+++ src/decimal.ts
@@ -1,33 +1,56 @@
 import type { Iterator } from './iterator.js';
-import {
-  readBigUintv,
-  readSmallUintv,
-  writeBigUintv,
-  writeSmallUintv,
-} from './uintv.js';
+import { readBigUintv, writeBigUintv } from './uintv.js';
 
 export const readDecimal = (iterator: Iterator) => {
-  const significandLength = readSmallUintv(iterator);
+  // Read the sign and the length of the significand:
+  const byte = iterator[0].getUint8(iterator[1]++);
+  const sign = byte & 0b1000_0000;
+  const significandLength = byte & 0b0111_1111;
+
+  // Read the integer representation of the value:
   const integer = readBigUintv(iterator);
-  if (significandLength === 0) return Number(integer);
-  const string = String(integer);
-  return +`${string.slice(0, -significandLength)}.${string.slice(-significandLength)}`;
+
+  // Convert the integer representation to a floating-point number value:
+  let absoluteValue;
+  if (significandLength === 0) {
+    absoluteValue = Number(integer);
+  } else {
+    const string = String(integer);
+    absoluteValue =
+      +`${string.slice(0, -significandLength)}.${string.slice(-significandLength)}`;
+  }
+
+  // Apply the sign to the value:
+  return sign === 0 ? absoluteValue : -absoluteValue;
 };
 
 export const writeDecimal = (iterator: Iterator, value: number) => {
+  // Get the sign bit:
+  const sign = value < 0 ? 0b1000_0000 : 0;
+
+  // Remove the sign from the value:
+  if (sign !== 0) value = -value;
+
   const string = String(value);
   const stringLength = string.length;
   let concatenatedInteger: string;
   let significandLength = 0;
 
+  // Find the decimal point:
   for (let i = 1; i < stringLength; i++) {
     if (string[i] === '.') {
+      // Get the length of the significand:
       significandLength = stringLength - i - 1;
+
+      // Remove the decimal point from the value:
       concatenatedInteger = `${string.slice(0, i)}${string.slice(i + 1)}`;
       break;
     }
   }
 
-  writeSmallUintv(iterator, significandLength);
+  // Write the sign bit and the length of the significand (up to 127 digits):
+  iterator[0].setUint8(iterator[1]++, sign | (significandLength & 0b0111_1111));
+
+  // Write the value represented as an integer:
   writeBigUintv(iterator, BigInt(concatenatedInteger! ?? string));
 };
