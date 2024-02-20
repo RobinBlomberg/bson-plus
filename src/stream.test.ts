c1dc80a38a95420e6e324a075c5cab4c2b89907c
Robin Blomberg <hello@robinblomberg.se>
2024-02-20 07:39:04 +0100

    feat: implement new stream object

---
+++ src/stream.test.ts
@@ -0,0 +1,105 @@
+import { strictEqual, throws } from 'assert';
+import { describe, test } from 'vitest';
+import type { Processor, Processors } from './stream.js';
+import { createStream } from './stream.js';
+
+describe('stream', () => {
+  const buffer = new ArrayBuffer(32);
+  const dataView = new DataView(buffer);
+  const stream = createStream(dataView);
+
+  const fail = <Type extends keyof Processors>(
+    type: Type,
+    input: Processors[Type] extends Processor<infer Input> ? Input : never,
+  ) => {
+    stream.setOffset(0);
+    throws(() => stream.write(type, input));
+  };
+
+  const ok = <Type extends keyof Processors>(
+    type: Type,
+    input: Processors[Type] extends Processor<infer Input> ? Input : never,
+    expectedLength: number,
+  ) => {
+    stream.setOffset(0);
+    stream.write(type, input);
+    const writeLength = stream.getOffset();
+    stream.setOffset(0);
+    const output = stream.read(type);
+    const readLength = stream.getOffset();
+    strictEqual(Object.is(input, -0) ? 0 : input, output, 'unexpected output');
+    strictEqual(writeLength, expectedLength, 'unexpected writeLength');
+    strictEqual(readLength, expectedLength, 'unexpected readLength');
+  };
+
+  test('bigint', () => {
+    for (const sign of [1n, -1n]) {
+      ok('bigint', sign * 0n, 1);
+      ok('bigint', sign * 63n, 1);
+      ok('bigint', sign * 64n, 2);
+      ok('bigint', sign * 8191n, 2);
+      ok('bigint', sign * 8192n, 3);
+      ok('bigint', sign * 1_048_575n, 3);
+      ok('bigint', sign * 1_048_576n, 4);
+      ok('bigint', sign * 134_217_727n, 4);
+      ok('bigint', sign * 134_217_728n, 5);
+      ok('bigint', sign * 2_147_483_647n, 5);
+      ok('bigint', sign * 2_147_483_648n, 5);
+      ok('bigint', sign * BigInt(Number.MAX_SAFE_INTEGER), 8);
+    }
+  });
+
+  test('biguint', () => {
+    ok('biguint', 0n, 1);
+    ok('biguint', 127n, 1);
+    ok('biguint', 128n, 2);
+    ok('biguint', 16_383n, 2);
+    ok('biguint', 16_384n, 3);
+    ok('biguint', 2_097_151n, 3);
+    ok('biguint', 2_097_152n, 4);
+    ok('biguint', 268_435_455n, 4);
+    ok('biguint', 268_435_456n, 5);
+    ok('biguint', 2_147_483_647n, 5);
+    ok('biguint', 2_147_483_648n, 5);
+    ok('biguint', BigInt(Number.MAX_SAFE_INTEGER), 8);
+  });
+
+  test('decimal', () => {
+    ok('decimal', 0, 2);
+    ok('decimal', 13_579.2468, 5);
+    ok('decimal', 3.141_592_653_589_793, 9);
+    // eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+    ok('decimal', 9_876_543_210.012_345_678_9, 9);
+    ok('decimal', Number.MAX_SAFE_INTEGER, 9);
+  });
+
+  test('int', () => {
+    for (const sign of [1, -1]) {
+      ok('int', sign * 0, 1);
+      ok('int', sign * 63, 1);
+      ok('int', sign * 64, 2);
+      ok('int', sign * 8191, 2);
+      ok('int', sign * 8192, 3);
+      ok('int', sign * 1_048_575, 3);
+      ok('int', sign * 1_048_576, 4);
+      ok('int', sign * 134_217_727, 4);
+      ok('int', sign * 134_217_728, 5);
+      ok('int', sign * 2_147_483_647, 5);
+      fail('int', sign * 2_147_483_648);
+    }
+  });
+
+  test('uint', () => {
+    ok('uint', 0, 1);
+    ok('uint', 127, 1);
+    ok('uint', 128, 2);
+    ok('uint', 16_383, 2);
+    ok('uint', 16_384, 3);
+    ok('uint', 2_097_151, 3);
+    ok('uint', 2_097_152, 4);
+    ok('uint', 268_435_455, 4);
+    ok('uint', 268_435_456, 5);
+    ok('uint', 2_147_483_647, 5);
+    fail('uint', 2_147_483_648);
+  });
+});
--- src/stream.ts
+++ src/stream.ts
@@ -1,148 +1,292 @@
-import { readBitset, writeBitset } from './bitset.js';
-import { readDecimal, writeDecimal } from './decimal.js';
-import type { InputFor } from './input.js';
-import {
-  readBigInt,
-  readInt,
-  readSmallInt,
-  writeBigInt,
-  writeInt,
-  writeSmallInt,
-} from './int.js';
-import type { Iterator } from './iterator.js';
-import type { Schema } from './schema.js';
-import { Type } from './type.js';
-import {
-  readBigUint,
-  readSmallUint,
-  readUint,
-  writeBigUint,
-  writeSmallUint,
-  writeUint,
-} from './uint.js';
+export type Processor<
+  Input extends bigint | number,
+  Output extends bigint | number = Input,
+> = [() => Output, (value: Input) => void];
 
-export const read = (iterator: Iterator, schema: Schema) => {
-  switch (schema.type) {
-    case Type.BigInt:
-      return readBigInt(iterator);
-    case Type.BigInt64: {
-      const value = iterator[0].getBigInt64(iterator[1]);
-      iterator[1] += 8;
-      return value;
-    }
-    case Type.BigUint:
-      return readBigUint(iterator);
-    case Type.BigUint64: {
-      const value = iterator[0].getBigUint64(iterator[1]);
-      iterator[1] += 8;
-      return value;
-    }
-    case Type.Bitset:
-      return readBitset(iterator, schema.valueLengths);
-    case Type.Decimal:
-      return readDecimal(iterator);
-    case Type.Float32: {
-      const value = iterator[0].getFloat32(iterator[1]);
-      iterator[1] += 4;
-      return value;
-    }
-    case Type.Float64: {
-      const value = iterator[0].getFloat64(iterator[1]);
-      iterator[1] += 8;
-      return value;
-    }
-    case Type.Int:
-      return readInt(iterator);
-    case Type.Int8:
-      return iterator[0].getInt8(iterator[1]++);
-    case Type.Int16: {
-      const value = iterator[0].getInt16(iterator[1]);
-      iterator[1] += 2;
-      return value;
-    }
-    case Type.Int32: {
-      const value = iterator[0].getInt32(iterator[1]);
-      iterator[1] += 4;
-      return value;
-    }
-    case Type.SmallInt:
-      return readSmallInt(iterator);
-    case Type.SmallUint:
-      return readSmallUint(iterator);
-    case Type.Uint:
-      return readUint(iterator);
-    case Type.Uint8:
-      return iterator[0].getUint8(iterator[1]++);
-    case Type.Uint16: {
-      const value = iterator[0].getUint16(iterator[1]);
-      iterator[1] += 2;
-      return value;
-    }
-    case Type.Uint32: {
-      const value = iterator[0].getUint32(iterator[1]);
-      iterator[1] += 4;
-      return value;
-    }
-  }
+export type Processors = {
+  bigint: Processor<bigint>;
+  bigint64: Processor<bigint>;
+  biguint: Processor<bigint>;
+  biguint64: Processor<bigint>;
+  decimal: Processor<bigint | number, number>;
+  float32: Processor<number>;
+  float64: Processor<number>;
+  int: Processor<number>;
+  int8: Processor<number>;
+  int16: Processor<number>;
+  int32: Processor<number>;
+  uint: Processor<number>;
+  uint8: Processor<number>;
+  uint16: Processor<number>;
+  uint32: Processor<number>;
 };
 
-export const write = <ThisSchema extends Schema>(
-  iterator: Iterator,
-  schema: ThisSchema,
-  input: InputFor[ThisSchema['type']],
-) => {
-  switch (schema.type) {
-    case Type.BigInt:
-      return writeBigInt(iterator, input as bigint);
-    case Type.BigInt64:
-      iterator[0].setBigInt64(iterator[1], input as bigint);
-      iterator[1] += 8;
-      break;
-    case Type.BigUint:
-      return writeBigUint(iterator, input as bigint);
-    case Type.BigUint64:
-      iterator[0].setBigUint64(iterator[1], input as bigint);
-      iterator[1] += 8;
-      break;
-    case Type.Bitset:
-      return writeBitset(iterator, schema.valueLengths, input as number[]);
-    case Type.Decimal:
-      return writeDecimal(iterator, input as number);
-    case Type.Float32:
-      iterator[0].setFloat32(iterator[1], input as number);
-      iterator[1] += 4;
-      break;
-    case Type.Float64:
-      iterator[0].setFloat64(iterator[1], input as number);
-      iterator[1] += 8;
-      break;
-    case Type.Int:
-      return writeInt(iterator, input as number);
-    case Type.Int8:
-      return iterator[0].setInt8(iterator[1]++, input as number);
-    case Type.Int16:
-      iterator[0].setInt16(iterator[1], input as number);
-      iterator[1] += 2;
-      break;
-    case Type.Int32:
-      iterator[0].setInt32(iterator[1], input as number);
-      iterator[1] += 4;
-      break;
-    case Type.SmallInt:
-      return writeSmallInt(iterator, input as number);
-    case Type.SmallUint:
-      return writeSmallUint(iterator, input as number);
-    case Type.Uint:
-      return writeUint(iterator, input as number);
-    case Type.Uint8:
-      return iterator[0].setUint8(iterator[1]++, input as number);
-    case Type.Uint16:
-      iterator[0].setUint16(iterator[1], input as number);
-      iterator[1] += 2;
-      break;
-    case Type.Uint32:
-      iterator[0].setUint32(iterator[1], input as number);
-      iterator[1] += 4;
-      break;
-  }
+export const createStream = (dataView: DataView, offset = 0) => {
+  const intRangeError = new RangeError(
+    "Value of type 'int' must be an integer from -2147483647 to 2147483647.",
+  );
+  const uintRangeError = new RangeError(
+    "Value of type 'uint' must be an integer from 0 to 2147483647.",
+  );
+
+  const processors: Processors = {
+    bigint: [
+      () => {
+        let byte = dataView.getUint8(offset++);
+        const sign = byte & 0b0100_0000;
+        let value = BigInt(byte & 0b0011_1111);
+        let shift = 6n;
+        while ((byte & 0b1000_0000) !== 0) {
+          byte = dataView.getUint8(offset++);
+          value |= BigInt(byte & 0b0111_1111) << shift;
+          shift += 7n;
+        }
+        return sign === 0 ? value : -value;
+      },
+      (value) => {
+        const sign = value < 0 ? 0b0100_0000 : 0;
+        if (sign !== 0) value = -value;
+        let byte = (Number(value) & 0b0011_1111) | sign;
+        value >>= 6n;
+        while (true) {
+          if (value !== 0n) byte |= 0b1000_0000;
+          dataView.setUint8(offset++, byte);
+          if (value === 0n) return;
+          byte = Number(value) & 0b0111_1111;
+          value >>= 7n;
+        }
+      },
+    ],
+    bigint64: [
+      () => {
+        const value = dataView.getBigInt64(offset);
+        offset += 8;
+        return value;
+      },
+      (value) => {
+        dataView.setBigInt64(offset, value);
+        offset += 8;
+      },
+    ],
+    biguint: [
+      () => {
+        let shift = 0n;
+        let value = 0n;
+        while (true) {
+          const byte = dataView.getUint8(offset++);
+          value |= BigInt(byte & 0b0111_1111) << shift;
+          if ((byte & 0b1000_0000) === 0) return value;
+          shift += 7n;
+        }
+      },
+      (value) => {
+        do {
+          let byte = Number(value & 0b0111_1111n);
+          value >>= 7n;
+          if (value !== 0n) byte |= 0b1000_0000;
+          dataView.setUint8(offset++, byte);
+        } while (value !== 0n);
+      },
+    ],
+    biguint64: [
+      () => {
+        const value = dataView.getBigUint64(offset);
+        offset += 8;
+        return value;
+      },
+      (value) => {
+        dataView.setBigUint64(offset, value);
+        offset += 8;
+      },
+    ],
+    decimal: [
+      () => {
+        const byte = dataView.getUint8(offset++);
+        const sign = byte & 0b1000_0000;
+        const significandLength = byte & 0b0111_1111;
+        const digits = read('biguint');
+        let abs;
+        if (significandLength === 0) {
+          abs = Number(digits);
+        } else {
+          const string = String(digits);
+          abs =
+            +`${string.slice(0, -significandLength)}.${string.slice(-significandLength)}`;
+        }
+        return sign === 0 ? abs : -abs;
+      },
+      (value) => {
+        const sign = (typeof value === 'bigint' ? value < 0n : value < 0)
+          ? 0b1000_0000
+          : 0;
+        const string = String(sign === 0 ? value : -value);
+        const stringLength = string.length;
+        let digits = string;
+        let significandLength = 0;
+        for (let i = 1; i < stringLength; i++) {
+          if (string[i] === '.') {
+            significandLength = stringLength - i - 1;
+            digits = `${string.slice(0, i)}${string.slice(i + 1)}`;
+            break;
+          }
+        }
+        dataView.setUint8(offset++, sign | (significandLength & 0b0111_1111));
+        write('biguint', BigInt(digits));
+      },
+    ],
+    float32: [
+      () => {
+        const value = dataView.getFloat32(offset);
+        offset += 4;
+        return value;
+      },
+      (value) => {
+        dataView.setFloat32(offset, value);
+        offset += 4;
+      },
+    ],
+    float64: [
+      () => {
+        const value = dataView.getFloat64(offset);
+        offset += 8;
+        return value;
+      },
+      (value) => {
+        dataView.setFloat64(offset, value);
+        offset += 8;
+      },
+    ],
+    int: [
+      () => {
+        let byte = dataView.getUint8(offset++);
+        const sign = byte & 0b0100_0000;
+        let value = byte & 0b0011_1111;
+        let shift = 6;
+        while ((byte & 0b1000_0000) !== 0) {
+          byte = dataView.getUint8(offset++);
+          value |= (byte & 0b0111_1111) << shift;
+          shift += 7;
+        }
+        return sign === 0 ? value : -value;
+      },
+      (value) => {
+        if (value <= -2_147_483_648 || value >= 2_147_483_648) {
+          throw intRangeError;
+        }
+        const sign = value < 0 ? 0b0100_0000 : 0;
+        if (sign !== 0) value = -value;
+        let byte = (value & 0b0011_1111) | sign;
+        value >>= 6;
+        while (true) {
+          if (value !== 0) byte |= 0b1000_0000;
+          dataView.setUint8(offset++, byte);
+          if (value === 0) return;
+          byte = value & 0b0111_1111;
+          value >>= 7;
+        }
+      },
+    ],
+    int8: [
+      () => {
+        return dataView.getInt8(offset++);
+      },
+      (value) => {
+        dataView.setInt8(offset++, value);
+      },
+    ],
+    int16: [
+      () => {
+        const value = dataView.getInt16(offset);
+        offset += 2;
+        return value;
+      },
+      (value) => {
+        dataView.setInt16(offset, value);
+        offset += 2;
+      },
+    ],
+    int32: [
+      () => {
+        const value = dataView.getInt32(offset);
+        offset += 4;
+        return value;
+      },
+      (value) => {
+        dataView.setInt32(offset, value);
+        offset += 4;
+      },
+    ],
+    uint: [
+      () => {
+        let shift = 0;
+        let value = 0;
+        while (true) {
+          const byte = dataView.getUint8(offset++);
+          value |= (byte & 0b0111_1111) << shift;
+          if ((byte & 0b1000_0000) === 0) return value;
+          shift += 7;
+        }
+      },
+      (value) => {
+        if (value >= 2_147_483_648) throw uintRangeError;
+        do {
+          let byte = value & 0b0111_1111;
+          value >>= 7;
+          if (value !== 0) byte |= 0b1000_0000;
+          dataView.setUint8(offset++, byte);
+        } while (value !== 0);
+      },
+    ],
+    uint8: [
+      () => {
+        return dataView.getUint8(offset++);
+      },
+      (value) => {
+        dataView.setUint8(offset++, value);
+      },
+    ],
+    uint16: [
+      () => {
+        const value = dataView.getUint16(offset);
+        offset += 2;
+        return value;
+      },
+      (value) => {
+        dataView.setUint16(offset, value);
+        offset += 2;
+      },
+    ],
+    uint32: [
+      () => {
+        const value = dataView.getUint32(offset);
+        offset += 4;
+        return value;
+      },
+      (value) => {
+        dataView.setUint32(offset, value);
+        offset += 4;
+      },
+    ],
+  };
+
+  const getOffset = () => {
+    return offset;
+  };
+
+  const read = <Type extends keyof Processors>(type: Type) => {
+    return processors[type][0]() as Parameters<Processors[Type][1]>[0];
+  };
+
+  const setOffset = (newOffset: number) => {
+    offset = newOffset;
+  };
+
+  const write = <Type extends keyof Processors>(
+    type: Type,
+    value: Parameters<Processors[Type][1]>[0],
+  ) => {
+    return processors[type][1](value as never);
+  };
+
+  return { getOffset, read, setOffset, write };
 };
